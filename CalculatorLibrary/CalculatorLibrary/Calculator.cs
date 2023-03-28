124ef221b1880edcc1f67065ed9af6852e1eaf72
236547 Janicki Marcin <236547@edu.p.lodz.pl>
2023-03-28 19:36:35 +0200

    Starting project

---
+++ CalculatorLibrary/CalculatorLibrary/Calculator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace CalculatorLibrary
+{
+    public class Calculator
+    {
+        double num1, num2;
+
+        public Calculator(double num1, double num2)
+        {
+            this.num1 = num1;
+            this.num2 = num2;
+        }
+
+        public double Add() { return num1 + num2; }
+
+        public double Sub() { return num1 - num2; }
+
+        public double Mul() { return num1 * num2; }
+
+        public double Div() { return num1 / num2; }
+
+        public double Mod() { return num1 % num2; }
+
+
+
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# University-ConcurrentProgramming
+# Concurrent programming
+
+## Working Group
+
+| Name Surname (initials) | GUID                                     |
+| ----------------------- | ---------------------------------------- |
+| Igor M                  | `{C5BCFC42-2E45-4D09-927C-00AC1211600A}` |
+| Marcin J                | `{2DCF4E9E-3020-493A-850D-842FCA0D0ED6}` |
