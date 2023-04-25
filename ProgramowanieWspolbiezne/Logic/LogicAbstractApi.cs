89943a9b42329260ed12b485d05d9b89b31dbf7f
236547 Janicki Marcin <236547@edu.p.lodz.pl>
2023-04-25 21:02:20 +0200

    Vector2 and Logic

---
+++ ProgramowanieWspolbiezne/Logic/LogicAbstractApi.cs
@@ -0,0 +1,11 @@
+﻿using Data;
+namespace Logic
+{
+    public abstract class LogicAbstractApi
+    {
+        protected LogicAbstractApi CreateLogicApi(DataAbstractApi data = default)
+        {
+            return new LogicApi(data ?? DataAbstractApi.CreateDataApi());
+        }
+    }
+}
---
+++ ProgramowanieWspolbiezne/Logic/LogicApi.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Data;
+
+namespace Logic
+{
+    public class LogicApi : LogicAbstractApi
+    {
+        private readonly DataAbstractApi _data;
+        public LogicApi(DataAbstractApi data)
+        {
+            _data = data;
+        }
+    }
+}
