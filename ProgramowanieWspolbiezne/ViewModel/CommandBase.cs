06bd922e5d8ef14586cdf4db9da064f076abf29c
236547 Janicki Marcin <236547@edu.p.lodz.pl>
2023-04-25 21:15:09 +0200

    ViewModel fixes

---
+++ ProgramowanieWspolbiezne/ViewModel/CommandBase.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace ViewModel
+{
+    public abstract class CommandBase : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+
+        public virtual bool CanExecute(object param) => true;
+        public abstract void Execute(object param);
+        protected void OnCanExecuteChanged()
+        {
+            CanExecuteChanged?.Invoke(this, new EventArgs());
+        }
+    }
+}
---
+++ ProgramowanieWspolbiezne/ViewModel/SimulationViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Model;
+
+
+namespace ViewModel
+{
+    public class SimulationViewModel : ViewModelBase
+    {
+        public IEnumerable<BallModel> Balls => _balls;
+        private readonly ObservableCollection<BallModel> _balls;
+        private LogicModel _logic;
+        private int _ballsCount;
+
+        public SimulationViewModel(LogicModel logic)
+        {
+            _logic = logic;
+        }
+
+        public int BallsCount
+        {
+            get => _ballsCount;
+            set
+            {
+                _ballsCount = value;
+                RaisePropertyChanged(nameof(BallsCount));
+            }
+        }
+
+        public CommandBase StartSimulation { get; }
+    }
+}
